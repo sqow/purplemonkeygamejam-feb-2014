8a321ce22734c83417faddeff671cefb7f20f4a3
Kyle Kellogg <kyle@kylekellogg.com>
2014-02-22 17:21:34 -0500

    Finished up the animations

--- view/character.lua
+++ view/character.lua
@@ -24,18 +24,44 @@ Character.State = {
     frame = 0,
     spriteWidth = 1,
     spriteHeight = 1
+  },
+  Attacking = {
+    image = nil,
+    batch = nil,
+    quads = {},
+    frame = 0,
+    spriteWidth = 1,
+    spriteHeight = 1
+  },
+  Damage = {
+    image = nil,
+    batch = nil,
+    quads = {},
+    frame = 0,
+    spriteWidth = 1,
+    spriteHeight = 1
+  },
+  Death = {
+    image = nil,
+    batch = nil,
+    quads = {},
+    frame = 0,
+    spriteWidth = 1,
+    spriteHeight = 1
   }
 }
 
 Character.__name = 'Character'
 
 local framerate, time = 1 / 12, 0
+local deathAnimCounter = 0
 
 local keyDown = {
   left = false,
   right = false,
   down = false,
-  up = false
+  up = false,
+  attack = false
 }
 
 function Character:__init( x, y, width, height, state )
@@ -44,8 +70,11 @@ function Character:__init( x, y, width, height, state )
   self.width = width or 70
   self.height = height or 70
 
-  self:setupState( Character.State.Standing, 'assets/images/stand.png', 6, 42, 66 )
-  self:setupState( Character.State.Walking, 'assets/images/walk.png', 6, 42, 66 )
+  self:setupState( Character.State.Standing, 'assets/images/stand.png', 6, 80, 75 )
+  self:setupState( Character.State.Walking, 'assets/images/walk.png', 6, 80, 75 )
+  self:setupState( Character.State.Attacking, 'assets/images/attack.png', 9, 80, 75 )
+  self:setupState( Character.State.Damage, 'assets/images/hurt.png', 5, 80, 75 )
+  self:setupState( Character.State.Death, 'assets/images/death.png', 2, 80, 75 )
 
   self:setState( state or Character.State.Standing )
 end
@@ -67,10 +96,22 @@ end
 function Character:setState( state )
   self.state = state or Character.State.Standing
   self.state.frame = 1
+
+  if self.state ~= Character.State.Standing or self.state ~= Character.State.Walking then
+    keyDown.up = false
+    keyDown.left = false
+    keyDown.down = false
+    keyDown.right = false
+  end
 end
 
 function Character:getState()
   return self.state
+end
+
+function Character:canMove()
+  local st = self:getState()
+  return st ~= Character.State.Attacking and st ~= Character.State.Damage and st ~= Character.State.Death
 end
 
 function Character:update( dt )
@@ -101,6 +142,19 @@ function Character:update( dt )
   if time >= framerate then
     self.state.frame = (self.state.frame % #self.state.quads) + 1
     time = 0
+    if self.state.frame <= 1 then
+      if keyDown.attack or self:getState() == Character.State.Damage then
+        keyDown.attack = false
+        self:setState( Character.State.Standing )
+      elseif self:getState() == Character.State.Death then
+        -- You dead, sucka
+        deathAnimCounter = deathAnimCounter + 1
+        if deathAnimCounter >= 5 then
+          --  End game here
+          self:setState( Character.State.Standing )
+        end
+      end
+    end
   end
 end
 
@@ -109,35 +163,42 @@ function Character:draw()
 end
 
 function Character:keypressed( key, isrepeat )
-  if key == 'w' or key == 'up' then
-    if self.state ~= Character.State.Walking then self:setState( Character.State.Walking ) end
-    keyDown.up = true
-  elseif key == 'a' or key == 'left' then
-    if self.state ~= Character.State.Walking then self:setState( Character.State.Walking ) end
-    keyDown.left = true
-  elseif key == 's' or key == 'down' then
-    if self.state ~= Character.State.Walking then self:setState( Character.State.Walking ) end
-    keyDown.down = true
-  elseif key == 'd' or key == 'right' then
-    if self.state ~= Character.State.Walking then self:setState( Character.State.Walking ) end
-    keyDown.right = true
+  if self:canMove() then
+    if key == 'w' or key == 'up' then
+      if self.state ~= Character.State.Walking then self:setState( Character.State.Walking ) end
+      keyDown.up = true
+    elseif key == 'a' or key == 'left' then
+      if self.state ~= Character.State.Walking then self:setState( Character.State.Walking ) end
+      keyDown.left = true
+    elseif key == 's' or key == 'down' then
+      if self.state ~= Character.State.Walking then self:setState( Character.State.Walking ) end
+      keyDown.down = true
+    elseif key == 'd' or key == 'right' then
+      if self.state ~= Character.State.Walking then self:setState( Character.State.Walking ) end
+      keyDown.right = true
+    elseif key == ' ' then
+      keyDown.attack = true
+      self:setState( Character.State.Attacking )
+    elseif key == 'kpenter' or key == 'return' then
+      self:setState( self:getState() ~= Character.State.Damage and Character.State.Damage or Character.State.Standing )
+    elseif key == 'z' then
+      self:setState( self:getState() ~= Character.State.Death and Character.State.Death or Character.State.Standing )
+    end
   end
 end
 
 function Character:keyreleased( key )
   if key == 'w' or key == 'up' then
-    if self.state ~= Character.State.Standing and not keyDown.left and not keyDown.right and not keyDown.down then self:setState( Character.State.Standing ) end
+    if self:canMove() and self.state ~= Character.State.Standing and not keyDown.left and not keyDown.right and not keyDown.down then self:setState( Character.State.Standing ) end
     keyDown.up = false
   elseif key == 'a' or key == 'left' then
-    if self.state ~= Character.State.Standing and not keyDown.up and not keyDown.right and not keyDown.down then self:setState( Character.State.Standing ) end
+    if self:canMove() and self.state ~= Character.State.Standing and not keyDown.up and not keyDown.right and not keyDown.down then self:setState( Character.State.Standing ) end
     keyDown.left = false
   elseif key == 's' or key == 'down' then
-    if self.state ~= Character.State.Standing and not keyDown.up and not keyDown.left and not keyDown.right then self:setState( Character.State.Standing ) end
+    if self:canMove() and self.state ~= Character.State.Standing and not keyDown.up and not keyDown.left and not keyDown.right then self:setState( Character.State.Standing ) end
     keyDown.down = false
   elseif key == 'd' or key == 'right' then
-    if self.state ~= Character.State.Standing and not keyDown.up and not keyDown.left and not keyDown.down then self:setState( Character.State.Standing ) end
+    if self:canMove() and self.state ~= Character.State.Standing and not keyDown.up and not keyDown.left and not keyDown.down then self:setState( Character.State.Standing ) end
     keyDown.right = false
-  elseif key == 'return' then
-    self:setState( self:getState() == Character.State.Standing and Character.State.Walking or Character.State.Standing )
   end
 end
